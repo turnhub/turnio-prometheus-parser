9b7caf4a4f1bfca6353adc9deb1068bfe0930a86
Simon de Haan <simon@praekelt.org>
2020-10-16 11:08:45 +0200

    add organization to mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule PrometheusParser.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      organization: "turnio"
     ]
   end
 
